8e13e0695975161d56c4eb0f447af8b369f9dbaf
woopaca <jcw001031@gmail.com>
2022-08-11 20:41:38 +0900

    algorithm study

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int n, m;
+    public static StringBuilder sb = new StringBuilder();
+    public static ArrayList<Integer> list = new ArrayList<>();
+    public static boolean[] visited;
+
+    public static void combination(int depth, int start, ArrayList<Integer> arr) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(list.get(i)).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            list.add(arr.get(i));
+            combination(depth + 1, i, arr);
+            list.remove(list.size() - 1);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        ArrayList<Integer>[] list = new ArrayList[n];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n];
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
-            list[i] = new ArrayList<>();
+            arr.add(Integer.parseInt(st.nextToken()));
         }
+        Collections.sort(arr);
+        combination(0, 0, arr);
 
-        list[0].add(Integer.parseInt(br.readLine()));
-
-        for(int i=1;i<n;i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<i+1;j++){
-                int a = -1, b = -1;
-                int tmp = Integer.parseInt(st.nextToken());
-                if(j-1 >=0){
-                    a=list[i-1].get(j-1);
-                }
-                if(j < list[i-1].size()){
-                    b = list[i-1].get(j);
-                }
-                list[i].add(a>b?a+tmp:b+tmp);
-            }
-        }
-        System.out.println(Collections.max(list[n-1]));
+        System.out.println(sb);
     }
 }
