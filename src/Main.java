eef080caec2e6d6dd92c23b2872134e523945008
woopaca <jcw001031@gmail.com>
2022-08-22 23:29:30 +0900

    study

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
 import java.io.*;
 import java.util.*;
 
-class Node{
-    public Node left;
-    public Node right;
-    public void setLeftNode(Node node){
-        left = node;
-    }
-    public void setRightNode(Node node){
-        right = node;
-    }
-}
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0;i<n;i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<3;j++) {
-                String tmp = st.nextToken();
+        int t = Integer.parseInt(br.readLine());
 
-
+        while(t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] sticker = new int[2][n];
+            for (int i = 0; i < 2; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    sticker[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            map.put(1, 0);
+            map.put(2, 0);
+            map.put(3, 0);
+            map.put(4, 0);
+            int i = 0;
+            while (i <= n - 1) {
+                map.put(1, map.get(1)+sticker[0][i]);
+                map.put(2, map.get(2)+sticker[1][i]);
+                map.put(3, map.get(3)+sticker[0][i]);
+                map.put(4, map.get(4)+sticker[0][i]);
             }
         }
     }
