fbd53ea4f0cc35d6f327ea724938a500305daf80
woopaca <jcw001031@gmail.com>
2022-08-23 00:22:49 +0900

    dynamic programming

--- src/CodingTestSilver/DynamicProgramming/Sticker9465.java
+++ src/CodingTestSilver/DynamicProgramming/Sticker9465.java
@@ -1,7 +1,9 @@
+package CodingTestSilver.DynamicProgramming;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Sticker9465 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
