31ee7005dc06f70698babf298c915a29e54e8456
woopaca <jcw001031@gmail.com>
2022-09-11 15:23:42 +0900

    dynamic programming 14501

--- src/CodingTestSilver/DynamicProgramming/Retirement14501.java
+++ src/CodingTestSilver/DynamicProgramming/Retirement14501.java
@@ -1,7 +1,9 @@
+package CodingTestSilver.DynamicProgramming;
+
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Retirement14501 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
