91e18b49c23f45bcc27c51de8b502573ef813ba5
woopaca <jcw001031@gmail.com>
2022-09-14 00:22:10 +0900

    2xn tileing

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,24 @@
 import java.io.*;
-import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
+       int n = Integer.parseInt(br.readLine());
 
-        int tmp = 1;
-        for(int i=n;i>0;i--) {
-            for (int j = 0; j < i - 1; j++) {
-                bw.write(" ");
-            }
-            if (i != 1) {
-                for (int k = 0; k < tmp; k++) {
-                    if (k == 0 || k == tmp - 1) {
-                        bw.write('*');
-                    } else {
-                        bw.write(" ");
-                    }
-                }
-            }
-            else{
-                for(int k=0;k<tmp;k++){
-                    bw.write("*");
-                }
-            }
-            tmp += 2;
-            bw.write("\n");
-        }
-        bw.flush();
-        bw.close();
+       int[] dp = new int[n+1];
+
+       if(n>=1){
+           dp[1] = 1;
+       }
+       if(n>=2) {
+           dp[2] = 3;
+       }
+
+       for(int i=3;i<=n;i++){
+           dp[i] = dp[i-2]*2 + dp[i-1];
+           dp[i] %= 10007;
+       }
+        System.out.println(dp[n]%10007);
     }
 }
