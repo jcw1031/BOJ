90348117bb5b3baf50a214e3bacef45c035a4056
woopaca <jcw001031@gmail.com>
2022-11-21 19:46:22 +0900

    2주차 코테 스터디

--- src/CodingTestSilver/Graph/BOJ3182.java
+++ src/CodingTestSilver/Graph/BOJ3182.java
@@ -1,9 +1,10 @@
+package CodingTestSilver.Graph;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
-public class Main {
+public class BOJ3182 {
     private static int[] answer;
     private static boolean[] isVisited;
 
